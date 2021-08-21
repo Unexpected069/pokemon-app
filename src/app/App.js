cde69a01ae1f08f228d8d5186297fadb60704ae4
Walter Mata <matawalterbernard@gmail.com>
2021-08-21 13:11:11 +0800

    add themes, replace uri params id to pokemon name

--- src/app/App.js
+++ src/app/App.js
@@ -10,6 +10,7 @@ import PokemonContextProvider from './context/PokemonContext';
 
 
 const colors = {
+  
   brand: {
     900: "#1a365d",
     800: "#153e75",
@@ -17,7 +18,39 @@ const colors = {
   },
 }
 
-const theme = extendTheme({ colors })
+const theme = extendTheme({
+  
+  components: {
+    Button: {
+      // 1. We can update the base styles
+      baseStyle: {
+        fontWeight: "bold", // Normally, it is "semibold"
+      },
+      // 2. We can add a new button size or extend existing
+      sizes: {
+        xl: {
+          h: "56px",
+          fontSize: "lg",
+          px: "32px",
+        },
+      },
+
+      // 3. We can add a new visual variant
+      variants: {
+        "with-shadow": {
+        
+          boxShadow: "0 0 2px 2px #efdfde",
+          color:'teal.100'
+        },
+        // 4. We can override existing variants
+        solid: (props) => ({
+          bg: 'teal'//props.colorMode === "dark" ? "red.300" : "red.500",
+        }),
+      },
+    },
+  },
+  
+  colors })
 
 
 const App = () => {
@@ -32,7 +65,7 @@ const App = () => {
                 <Switch>
                 <Route exact path={'/'} render={() => { return <Redirect to={'/home'}/> }}/>
                 <Route exact path='/home' exact component={() => <Home  />} />
-                <Route exact path={'/pokemon/:id'} exact component={(props) => <PokemonDetails {...props} />} />
+                <Route exact path={'/pokemon/:pokemonName'} exact component={(props) => <PokemonDetails {...props} />} />
                 <Route exact path={'/myTeam'} exact component={() => <MyTeam />} /> 
                 
                 </Switch>
