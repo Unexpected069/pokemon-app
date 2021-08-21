365fde172fc8af3f3fa790a456b2b0eefe808550
Walter Mata <matawalterbernard@gmail.com>
2021-08-21 13:15:33 +0800

    add My Team label

--- src/app/pages/MyTeam/MyTeam.js
+++ src/app/pages/MyTeam/MyTeam.js
@@ -1,16 +1,19 @@
 import React, {Component, useState,useContext, useEffect} from 'react';
 import DataTable from '../../component/DataTable/DataTable';
 
-
   const MyTeam=()=>{
 
   return(
     <React.Fragment>
 
-   
     <div className="container" style={{paddingTop: '2rem'}} >
          <div className="column">
-           
+           <div style={{
+             fontSize:'24px',
+             fontWeight:'bold',
+             marginBottom:'5px'
+           }}>My Team</div>
+          
         <DataTable/>
    
         </div>
